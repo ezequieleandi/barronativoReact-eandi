0d4031247098ef0a1e70ec106a54e83336a07b81
ezequieleandi <eandiezequiel@gmail.com>
2022-08-08 11:47:49 -0300

    correccion estilado

--- src/components/cartItems/CartItems.js
+++ src/components/cartItems/CartItems.js
@@ -5,7 +5,7 @@ function CartItems({cart, removeItem}) {
    
   return (
     <>
-      <li className='list-group-item center liCart'>
+      <li className='center liCart'>
         {cart.item.producto} - {cart.item.linea} - ${cart.item.precio} x {cart.quantity}
         <button onClick={() => removeItem(cart.item.id)}>eliminar</button>
       </li>
